02fcf725ce0cf68f8154e6289a022734eda2af95
jessiBramwell <bramibly@gmail.com>
2019-02-24 16:15:20 -0700

    game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,97 @@
-var game = {
-    wordList: ['cat', 'dog', 'goat'],
-    guessesRemaining: 7,
-    guessMiss: [],
+// DOM elements
+var statsDOM = document.getElementById('stats');
+var sketchDOM = document.getElementById('sketch');
+var viewerDOM = document.getElementById('viewer');
+var startDOM = document.getElementById('start');
 
-    init: function () {
+var str = "abcdefghijklmnopqrstuvwxyz";
+var letters = str.split("");
+var wordList = ['lithium', 'magnesium', 'aluminum', 'titanium', 'iron', 'cobalt', 'nickel', 'copper', 'zinc', 'silver', 'tin', 'gold', 'mercury', 'lead'];
+var word;
+var wordSplit = [];
+var guess;
+var guesses = [];
+var letter;
+var currentWord;
 
-    },
+startDOM.onclick = init;
 
-    pickWord: function () {
-        var word = wordList[Math.floor(Math.random() * wordList.length)];
-
-    },
-
-    displayWord: function () {
-        // display word using placeholders
-        // update placeholders with letters
-
-    },
-
-    updateStats: function () {
-        // keep track of number of guesses
-        // display letters that are misses
+function init() {
+    if (currentWord) {
+        currentWord.remove();
     }
+    word = wordList[Math.floor(Math.random() * wordList.length)];
+    wordSplit = word.split('');
+    showHide(startDOM);
+    guesses = [];
+    correct = 0;
+    displayWord();
+    check();
 
 }
 
+function displayWord() {
+    console.log(wordSplit);
+    currentWord = document.createElement('ul');
 
+    for (var i = 0; i < word.length; i++) {
+        currentWord.setAttribute('id', 'current-word');
+        letter = document.createElement('li');
+        letter.setAttribute('class', 'letter');
+        letter.setAttribute('id', 'index-' + i);
+        letter.setAttribute('value', wordSplit[i]);
 
+        wordSplit.forEach(function () {
+            letter.innerHTML = '_';
+        });
 
-// allow the user to guess a letter
+        viewerDOM.appendChild(currentWord);
+        currentWord.appendChild(letter);
+    }
+}
 
+function check() {
+    document.onkeyup = function (event) {
+        guess = event.key;
+
+        if ((letters.includes(guess)) && (!guesses.includes(guess))) {
+
+            if (!word.includes(guess)) {
+                guesses.push(guess);
+                updateStats();
+
+                if (guesses.length === 10) {
+                    endGame();
+                }
+            } else {
+                for (var i = 0; i < word.length; i++) {
+                    if (word[i] === guess) {
+                        document.getElementById('index-' + i).innerHTML = guess;
+
+                    }
+                }
+            }
+
+            if (letter.getAttribute('value') === letter.innerHTML) {
+                endGame();
+            }
+        }
+    }
+}
+
+function updateStats() {
+    statsDOM.textContent = guesses;
+}
+
+function endGame() {
+    startDOM.textContent = 'New Word';
+    showHide(startDOM);
+}
+
+function showHide(el) {
+    if (el.style.display === "none") {
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }
+}
